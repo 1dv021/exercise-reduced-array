8e884cfd2ee071b43fff2dec14f7bb4d5f4e5dba
mtslck <mats.loock@lnu.se>
2016-09-15 14:44:07 +0200

    Initial commit

---
+++ src/reducedArray.js
@@ -0,0 +1,23 @@
+/**
+ * Module for reducedArray.
+ *
+ * @namespace bta
+ * @author John Häggerud
+ * @author Mats Loock
+ * @version 1.0.1
+ */
+
+'use strict';
+
+/**
+ * Takes an Array Numbers and returns the sum of all numbers in the array.
+ *
+ * @param {Array.<Number>} data An array with numbers
+ * @returns {Number} The total sum of the numbers in the array.
+ */let getTotal = function(data) {
+
+  // TODO: Write your code here.
+
+};
+
+module.exports.getTotal = getTotal;
